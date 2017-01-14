fa07351be20e16129c942fe36ab616e5b77595d7
pediehl <pediehl@googlemail.com>
2017-01-14 19:15:52 +0100

    sensoren im quellcode deutlicher gemacht print ausgabe angepasst

--- code/read_multiple_channels.py
+++ code/read_multiple_channels.py
@@ -3,8 +3,8 @@ import time
 adc = MCP3008()
 
 while True:
-    value = adc.read( channel = 0 )
+    wassersensor = adc.read( channel = 0 )
     poti = adc.read( channel = 1 )
-    print("Anliegende Spannung: %.2f V" % (value / 1023.0 * 3.3) )
+    print("Spannung Wassersensor: %.2f V" % (wassersensor / 1023.0 * 3.3) )
     print("Widerstand am Potentiometer: %.2f \u2126" % (poti / 1023) )
     time.sleep(3)
