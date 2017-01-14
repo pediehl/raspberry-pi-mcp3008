5e8102a6ea890ec7b724f69e5aa42d0f2a02cf9c
pediehl <pediehl@googlemail.com>
2017-01-15 00:04:33 +0100

    korrektur widerstand hat kohm an stelle von ohm

--- code/read_multiple_channels.py
+++ code/read_multiple_channels.py
@@ -6,5 +6,5 @@ while True:
     wassersensor = adc.read( channel = 0 )
     poti = adc.read( channel = 1 )
     print("Spannung Wassersensor: %.2f V" % (wassersensor / 1023.0 * 3.3) )
-    print("Widerstand am Potentiometer: %.2f \u2126" % (poti / 1023) )
+    print("Widerstand am Potentiometer: %.2f K\u2126" % (poti / 1023) )
     time.sleep(3)
