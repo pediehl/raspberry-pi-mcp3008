275a2ce260d77e573b49da89db897342aa90417e
pediehl <pediehl@googlemail.com>
2017-01-14 18:58:26 +0100

    code überarbeitet

--- code/read_multiple_channels.py
+++ code/read_multiple_channels.py
@@ -3,7 +3,7 @@ import time
 adc = MCP3008()
 
 while True:
-    value = adc.read( channel = 0 ) # Den auszulesenden Channel kannst du natürlich anpassen
+    value = adc.read( channel = 0 )
     poti = adc.read( channel = 1 )
     print("Anliegende Spannung: %.2f V" % (value / 1023.0 * 3.3) )
     print("Widerstand am Potentiometer: %.2f \u2126" % (poti / 1023) )
