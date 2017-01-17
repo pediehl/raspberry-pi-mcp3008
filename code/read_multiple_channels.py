d645466334202749c5b614d36b50378daf357da4
pediehl <pediehl@googlemail.com>
2017-01-17 21:51:17 +0100

    Python im Abschnitt "Potentiometer" die weitere Ausgabe in Ohm entfernt, da falsch & fehlerhaft

--- code/read_multiple_channels.py
+++ code/read_multiple_channels.py
@@ -7,5 +7,4 @@ while True:
     poti = adc.read( channel = 1 )
     print("Spannung Wassersensor: %.2f V" % (wassersensor / 1023.0 * 3.3) )
     print("Widerstand am Potentiometer: %.2f K\u2126" % (poti / 1023) )
-    print("Widerstand am Potentiometer: %.0f \u2126" % (poti / 1023.0 + 1000))
     time.sleep(3)
