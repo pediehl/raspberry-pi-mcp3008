3f4248856aa3548b9ab0fbb86aee9dd2fc39345e
pediehl <pediehl@googlemail.com>
2018-10-07 16:53:36 +0200

    Umstellung auf gpiozero SPI
    für das Potentiometer existieren zwei Beispiele für eine Prozent-Berechnung und eine Ohm-Berechnung.

---
+++ code/poti_ohm.py
@@ -0,0 +1,8 @@
+import time , gpiozero
+
+poti = gpiozero.MCP3008 (channel =0)
+
+while True :
+ wert =  int((poti.raw_value / 1023) * 1000)
+ print ("Der Poti steht auf" , wert , "Ohm")
+ time.sleep (1)
---
+++ code/poti_prozent.py
@@ -0,0 +1,8 @@
+import time , gpiozero
+
+poti = gpiozero.MCP3008 (channel =0)
+
+while True :
+ wert = int ( poti.value * 100)
+ print ("Der Poti steht auf" , wert , "%")
+ time.sleep (1)
